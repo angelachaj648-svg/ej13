5370eeac6629360432284de1320cead0fc7f6389
angela chaj <angelachaj648@gmail.com>
2026-03-13 01:04:12 -0500

    compra

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿double resultado13 = TotalCompra(10, 20, 30);
+Console.WriteLine("13 Resultado: " + resultado13);
+static double TotalCompra(double a, double b, double c)
+{
+	return a + b + c;
+}
